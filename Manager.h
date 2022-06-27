ac71e7ad21f2873261c19b97e27ab3e16667e9ae
GalinaRubo <107495306+GalinaRubo@users.noreply.github.com>
2022-06-27 14:13:36 +0700

    Update Manager.h
    File are contaned only methods of working with a document

--- Manager.h
+++ Manager.h
@@ -1,176 +1,47 @@
 
+
 #pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <io.h>  //библиотека для работы с файлами в плане доступа 
+#include <io.h>  //Р±РёР±Р»РёРѕС‚РµРєР° РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ С„Р°Р№Р»Р°РјРё РІ РїР»Р°РЅРµ РґРѕСЃС‚СѓРїР° 
 #include <windows.h>
+#include "Document.h"
 
 using namespace std;
-class ActDoc
+
+class FunDoc : virtual Document
 {
+	//РјРµС‚РѕРґС‹ СЂР°Р±РѕС‚С‹ СЃ РґРѕРєСѓРјРµРЅС‚РѕРј
 public:
-	string Path;
-
-	ActDoc()//конструктор с инициализацией
-	{
-		Path = SetPath();
-	}
-
-	//Функция ввода пути с клавиатуры
-	string SetPath()
-	{
-		cout << "Введите путь к документу: ";
-		getline(cin, Path);// ввод пути и имени файла с клавиатуры
-		while (CheckPath())
-		{
-			cout << "Задайте путь  к документу корректно: " << endl;
-			SetPath();
-		}
-		return Path;
-	}
-
-	//Функция проверки на корректность ввода пути к файлу
-	bool CheckPath()
-	{
-		WIN32_FILE_ATTRIBUTE_DATA;
-		const char* P = Path.data();
-		_finddata_t* InfoFile = new _finddata_t;
-		intptr_t Done = _findfirst(P, InfoFile);
-		if (InfoFile->attrib & _A_SUBDIR) //проверка не директория ли
-		{
-			Path.append("\\");
-			Path = AddName();
-			return 0;
-		}
-		if (Done == -1)
-		{
-			while (int pos = PosSimbol("\\"))
-			{
-				Path.erase(pos);
-				if (!CheckPath()) return 0;
-			};
-			printf("Путь задан неверно!\n");
-			return 1;
-		}
-		return 0;
-	}
-	//функция определения позиции  последней заданной подстроки  в строке
-	int PosSimbol(string str)
-	{
-		int i = 0;
-		int pos = 0;
-		//определение позиции последнего заданного символа  в строке
-		for (i = Path.find(str, i++); i != -1; i = Path.find(str, i + 1))
-		{
-			pos = i;
-			continue;
-		}
-		return pos;//возвращение позиции последнего символа
-	}
-	//функция задания имени файла в пути
-	string AddName()
-	{
-		string NewName;// новое имя файла
-		cout << "Задайте имя документа: ";
-		getline(cin, NewName);// имени файла с клавиатуры		
-		string NewPath = Path;
-		//извлечение пути из старого имени
-		int pos = 0;
-		pos = PosSimbol("\\");
-		NewPath.erase(pos + 1);
-		//добавление к пути нового имени и передача его в функцию rename
-		NewPath.append(NewName);
-		return NewPath;
-	}
-};
-
-
-class FunDoc : virtual  ActDoc
-{
-public:
-	//функция открытия документа
+	//С„СѓРЅРєС†РёСЏ РѕС‚РєСЂС‹С‚РёСЏ С„Р°Р№Р»Р° СЃ РґРѕРєСѓРјРµРЅС‚РѕРј
 	void Open()
-	{
-		ifstream Doc(Path);
-		if (Doc.is_open()) // вызов метода is_open()
-		{
-			cout << "Документ открыт!" << endl;
-		}
-		else
-		{
-			cout << "Ошибка при открытии документа!" << endl;
-		}
-	}
-	//Функция закрытия файла
+	{}
+	//Р¤СѓРЅРєС†РёСЏ Р·Р°РєСЂС‹С‚РёСЏ С„Р°Р№Р»Р° СЃ РґРѕРєСѓРјРµРЅС‚РѕРј
 	void Close()
-	{
-		ifstream Doc(Path);
-		if (Doc.is_open())
-		{
-			Doc.close();
-			cout << "Документ закрыт!" << endl;
-		}
-		else
-		{
-			cout << "Документ был закрыт!" << endl;
-		}
-	}
-	//Функция печати доумента
+	{}
+	//Р¤СѓРЅРєС†РёСЏ РїРµС‡Р°С‚Рё РґРѕСѓРјРµРЅС‚Р°
 	void Print()
-	{
-		ifstream Doc(Path);
-		cout << "Документ " << Path << " распечатан!" << endl;
-
-	}
-	//Функция создания документа 
+	{}
+	//Р¤СѓРЅРєС†РёСЏ СЃРѕР·РґР°РЅРёСЏ РґРѕРєСѓРјРµРЅС‚Р° 
 	void Create()
-	{
-		ifstream Doc(Path);
-		if (Doc.is_open())
-		{
-			cout << "Документ существует!";
-		}
-		else
-		{
-			ofstream Doc(Path);
-			cout << "Документ создан!\n\n";
-		}
-	}
-	//функция сохранения файла
+	{}
+	//С„СѓРЅРєС†РёСЏ СЃРѕС…СЂР°РЅРµРЅРёСЏ С„Р°Р№Р»Р°
 	void Save()
-	{
-		ifstream Doc(Path);
-		if (Doc.is_open())
-		{
-			Doc.close();
-			cout << "Документ сохранен!\n\n";
-		}
-		else
-		{
-			cout << "Документ был сохранен!\n\n";
-		}
-	}
-	//функция сохранения файла с новым именем
-	void Save_n()
-	{
-		ifstream Doc(Path);
-		if (Doc.is_open())
-		{
-			Doc.close();
-		}
-		Rename();
-	}
-
-	//Переименование файла
+	{}
+	//С„СѓРЅРєС†РёСЏ СЃРѕС…СЂР°РЅРµРЅРёСЏ С„Р°Р№Р»Р° СЃ РЅРѕРІС‹Рј РёРјРµРЅРµРј
+	void Save_as()
+	{}
+	//РџРµСЂРµРёРјРµРЅРѕРІР°РЅРёРµ С„Р°Р№Р»Р°
 	void Rename()
-	{
-		string NewPath;
-		NewPath = AddName();
-		rename(Path.data(), NewPath.data());
-		cout << "Документ успешно сохранен с новым именем!" << endl;
-	}
+	{}
 };
+
+
+
+
+
+
 
 
 
